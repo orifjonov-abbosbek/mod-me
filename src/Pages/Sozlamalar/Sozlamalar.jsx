f9d87323c9cdb36163eb9d19f1c43fdbfc0b80e6
orifjonov-abbosbek <zloy19781999@gmail.com>
2023-05-24 10:46:54 +0500

    second commit

--- src/Pages/Sozlamalar/Sozlamalar.jsx
+++ src/Pages/Sozlamalar/Sozlamalar.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import undov from "../../assets/undov.svg";
 import redtrash from "../../assets/redtrash.svg";
+import cursor from "../../assets/cursor.svg";
+import qalam3 from "../../assets/qalam3.svg";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import { API_KEY } from "../../constants/api";
 
@@ -20,12 +22,24 @@ import {
   TabsBody,
   TabPanel,
   Textarea,
+  Switch,
 } from "@material-tailwind/react";
 
 const Sozlamalar = () => {
   const [activeTab, setActiveTab] = React.useState("xodimlar");
   const [openRight, setOpenRight] = React.useState(false);
   const [openRight2, setOpenRight2] = React.useState(false);
+  const [openRight3, setOpenRight3] = React.useState(false);
+
+  const [text, setText] = React.useState("");
+  const [characterCount, setCharacterCount] = React.useState(0);
+
+  const handleChange = (event) => {
+    const inputText = event.target.value;
+    setText(inputText);
+    setCharacterCount(inputText.length);
+  };
+
   const [rooms, setRooms] = React.useState([]);
 
   const [staff, setStaff] = React.useState([]);
@@ -40,6 +54,8 @@ const Sozlamalar = () => {
 
   const TABLE_HEAD2 = ["Id", "Nomi"];
 
+  const TABLE_HEAD3 = ["Nomi", "Amallar"];
+
   const handleTabChange = (value) => {
     setActiveTab(value);
   };
@@ -52,11 +68,19 @@ const Sozlamalar = () => {
     setOpenRight2(true);
   };
 
+  const openDrawerRight3 = () => {
+    setOpenRight3(true);
+  };
+
   const closeDrawerRight = () => {
     setOpenRight(false);
   };
   const closeDrawerRight2 = () => {
     setOpenRight2(false);
+  };
+
+  const closeDrawerRight3 = () => {
+    setOpenRight3(false);
   };
 
   const fetchStaffs = async () => {
@@ -85,20 +109,19 @@ const Sozlamalar = () => {
     const Rooms = await fetch(`${API_KEY}groups`);
 
     const data = await Rooms.json();
-     const modifiedData = data.data.map((groups, index) => ({
-       ...groups,
-       customId: index + 1,
-     }));
+    const modifiedData = data.data.map((groups, index) => ({
+      ...groups,
+      customId: index + 1,
+    }));
 
     setRooms(modifiedData);
+  };
 
-};
+  console.log(rooms);
 
-console.log(rooms)
-
-  useEffect(()=> {
+  useEffect(() => {
     fetchRooms();
-  }, [])
+  }, []);
 
   return (
     <>
@@ -229,7 +252,50 @@ console.log(rooms)
         <Button>Yuborish</Button>
       </Drawer>
 
-      <div className="relative  w-[100%] h-[100vh]">
+      <Drawer
+        className="p-4"
+        placement="right"
+        open={openRight3}
+        onClose={closeDrawerRight3}
+      >
+        <div className="head_modal mb-6 flex items-center justify-between">
+          <Typography
+            variant="h5"
+            color="gray"
+            className="flex items-center gap-2 font-semibold text-sm"
+          >
+            <img
+              style={{ width: "1.5rem", opacity: "0.5" }}
+              src={undov}
+              alt=""
+            />
+            Yangi xona qo'shish
+          </Typography>
+          <IconButton
+            variant="text"
+            color="blue-gray"
+            onClick={closeDrawerRight3}
+          >
+            <XMarkIcon strokeWidth={2} className="h-5 w-5" />
+          </IconButton>
+        </div>
+
+        <div className="mb-4">
+          <Textarea
+            label="Xabarni kiriting"
+            value={text}
+            onChange={handleChange}
+          />
+        </div>
+
+        <p className="text-sm mb-4 opacity-70">
+          Character Count: {characterCount}
+        </p>
+
+        <Button>Yuborish</Button>
+      </Drawer>
+
+      <div className="pr-2">
         <Tabs
           className="tab "
           id="custom-animation"
@@ -430,13 +496,171 @@ console.log(rooms)
                                 {item.room}
                               </Typography>
                             </td>
-                           
-                         
                           </tr>
                         );
                       })}
                     </tbody>
                   </table>
+                </div>
+              </div>
+            </TabPanel>
+
+            <TabPanel value="avtosms">
+              <div className="flex justify-between px-10">
+                <div className="">
+                  <h3 className="font-semibold mb-3">SMS Turi</h3>
+
+                  <ul className="type">
+                    <li>
+                      <p className="flex items-center">
+                        To'lov amalga oshirildi <img src={cursor} alt="" />
+                      </p>
+
+                      <Switch id="1" />
+                    </li>
+                    <li>
+                      <p className="flex items-center">
+                        Talaba guruhga qo'shildi <img src={cursor} alt="" />
+                      </p>
+
+                      <Switch id="2" />
+                    </li>
+                    <li>
+                      <p className="flex items-center">
+                        Talaba tug'ilgan kuni <img src={cursor} alt="" />
+                      </p>
+
+                      <Switch id="3" />
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="">
+                  <h3 className="font-semibold mb-3">SMS matn:</h3>
+
+                  <Textarea
+                    className="bg-white w-[300px] mb-3"
+                    label="SMS matn"
+                  />
+
+                  <h3 className="font-semibold mb-4"> Yuborilgan SMS misoli</h3>
+
+                  <p className="m-5 ">SMS matn</p>
+                  <div className=" flex justify-end">
+                    <Button>Saqlash</Button>
+                  </div>
+                </div>
+
+                <div className="">
+                  <h3 className="font-semibold mb-3">Tavsif</h3>
+
+                  <ul className="desc pr-10">
+                    <li>
+                      <p className="font-semibold mb-3">
+                        Mavjud o'zgaruvchilar
+                      </p>
+                    </li>
+                    <li>
+                      <p className="mb-2">(STUDENT) - talabaning ismi</p>
+                      <p className="mb-2">(GROUP) - guruh nomi</p>
+                      <p className="mb-2">(SUM) - to'lob miqdori</p>
+                      <p className="mb-2">(LC) - o'quv markazingiz nomi</p>
+                      <p className="mb-2">(TEACHER) - auto-sms-teacher</p>
+                      <p className="mb-2">(TIME) - vaqt</p>
+                      <p className="mb-2">(ROOM) - xona</p>
+                      <p className="mb-2">(DAYS) - kunlar</p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </TabPanel>
+
+            <TabPanel value="smsshablonlar">
+              <div className="">
+                <div className="talabalar_head flex items-center text-2xl justify-between  pt-3 pb-4 pe-4  border-b-2 border-white mb-5">
+                  <h2 className="font-semibold">Xodimlar</h2>
+                  <Button onClick={openDrawerRight3}>Yangisini qo'shish</Button>
+                </div>
+
+                <div className=" bg-white flex flex-col gap-y-5 p-5 rounded-lg">
+                  <ul className="smsshablon bg-white   rounded-lg">
+                    <li className="flex justify-between font-semibold mb-4  border-b-1">
+                      <p>Nomi</p> <p>Amallar</p>
+                    </li>
+
+                    <li className="flex justify-between  mb-4">
+                      <p>
+                        Assalomu aleykusm, iltimos to'lovni o'z vaqtida amalga
+                        oshiring
+                      </p>
+                      <div className=" flex gap-x-5">
+                        <button>
+                          <img src={qalam3} alt="" />
+                        </button>
+                        <button>
+                          <img src={redtrash} alt="" />
+                        </button>
+                      </div>
+                    </li>
+                    <li className="flex justify-between mb-4">
+                      <p>
+                        Assalomu aleykum, webinarda qatnashganingizdan
+                        hursandmiz
+                      </p>
+                      <div className=" flex gap-x-5">
+                        <button>
+                          <img src={qalam3} alt="" />
+                        </button>
+                        <button>
+                          <img src={redtrash} alt="" />
+                        </button>
+                      </div>
+                    </li>
+
+                    <li className="flex justify-between mb-4">
+                      <p>
+                        Assalomu aleykum, siz kutayotgan guruh ochildi!
+                        Batafsil: https://
+                      </p>
+                      <div className=" flex gap-x-5">
+                        <button>
+                          <img src={qalam3} alt="" />
+                        </button>
+                        <button>
+                          <img src={redtrash} alt="" />
+                        </button>
+                      </div>
+                    </li>
+
+                    <li className="flex justify-between mb-4">
+                      <p>
+                        Assalomu aleykum, bugungi bayram bilan sizni o'z
+                        jamoamiz bilan qutlaymiz
+                      </p>
+                      <div className=" flex gap-x-5">
+                        <button>
+                          <img src={qalam3} alt="" />
+                        </button>
+                        <button>
+                          <img src={redtrash} alt="" />
+                        </button>
+                      </div>
+                    </li>
+                  </ul>
+
+                  <ul className="ul p-0">
+                    <li className="li">
+                      <span>Ma'lumotlar 15 dan 35 gacha, 234 ta dan</span>
+                    </li>
+                    <li className="li">
+                      <p>1</p>
+                      <p className="active">2</p>
+                      <p>3</p>
+                      <p>...</p>
+                      <p>15</p>
+                      <p>16</p>
+                    </li>
+                  </ul>
                 </div>
               </div>
             </TabPanel>
